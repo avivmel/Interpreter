4c5ba2bca708e6a4a245053ada38973e2b0febe5
Aviv Melamud <aviv@melamuds.com>
2020-06-23 12:36:50 -0400

    Initial Commit

---
+++ interpreter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  interpreter
+//
+//  Created by Aviv Melamud on 6/23/20.
+//  Copyright © 2020 Aviv Melamud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
